40bed74640ef7bf000a99eb17a1ae4ad951b5912
Luca Pulira <luca.pulira@gmail.com>
2023-12-04 18:03:54 +0100

    Simple pages structure

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <h2>Eum rerum quia est eius vitae rem voluptas dolorem. </h2><p>Lorem ipsum dolor sit amet. Aut pariatur sequi <a href="https://www.loremipzum.com" target="_blank">Ea quibusdam in iusto necessitatibus ut assumenda delectus</a> vel natus accusantium. Sit dolorem facere vel animi corruptiQui officia non possimus molestiae eum facere praesentium qui provident velit eum dolores sint. Aut sapiente voluptate et accusamus veniam <em>Qui quos ea ratione officiis At voluptas esse</em>. Ut sint iste ea saepe culpaSed sunt vel cupiditate autem ut quibusdam voluptatem et voluptatem sint. </p><h2>Quo repellendus quia ut reprehenderit mollitia. </h2><p>Quo consectetur quas est omnis velit <em>Ut architecto est expedita velit non porro molestiae aut ipsa odio</em>. Et fugiat undeNon voluptatem ea nesciunt pariatur qui repellendus natus eos neque voluptatem. Et quod nulla rem nulla dolores <strong>Ab nulla ut quam quia sed nobis delectus non sunt mollitia</strong>. Vel natus odit est beatae nemo33 repudiandae. </p><h3>33 mollitia labore aut inventore veniam. </h3><p>Ea dolores perspiciatis est molestias cupiditateaut voluptas rem laboriosam ducimus vel deleniti obcaecati. Et Quis dolores est corrupti consequatur <em>In delectus est eligendi error</em> ea eaque delectus non iste asperiores ut totam maiores. </p>
+  </main>
+  <footer>
+    <ul>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+    </ul>
+  </footer>
+</template>
+<script setup lang="ts">
+</script>
